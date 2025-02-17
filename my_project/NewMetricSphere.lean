a906280152d9a955f7b30ecb7d957b5461b7a066
Dominic Steinitz <dominic@steinitz.org>
2025-02-17 10:20:25 +0000

    Start proving continuity of local trivialisations

--- my_project/NewMetricSphere.lean
+++ my_project/NewMetricSphere.lean
@@ -6,7 +6,6 @@ open Complex
 #check ChartedSpace (EuclideanSpace ℝ (Fin 1)) ↑(Metric.sphere 0 1)
 #check ChartedSpace.chartAt (sorry : ↑(Metric.sphere 0 1))
 
-
 example : Metric.sphere (0 : EuclideanSpace ℝ (Fin 2)) 1 = {x | ∑ i : Fin 2, x i ^ 2 = 1 ^ 2} :=
    EuclideanSpace.sphere_zero_eq 1 (le_of_lt Real.zero_lt_one)
 
@@ -49,7 +48,6 @@ example (y : Fin 2 -> ℝ) : y ∈ Metric.sphere z 1 <-> dist y z = 1 := Metric.
 #check ChartedSpace (EuclideanSpace ℝ (Fin 1)) (Metric.sphere (0 : EuclideanSpace ℝ (Fin 2)) 1)
 #synth ChartedSpace (EuclideanSpace ℝ (Fin 1)) (Metric.sphere (0 : EuclideanSpace ℝ (Fin 2)) 1)
 
-
 #check chartAt (EuclideanSpace ℝ (Fin 1)) (sorry : ((Metric.sphere (0 : EuclideanSpace ℝ (Fin 2)) 1)))
 
 noncomputable def U := chartAt (EuclideanSpace ℝ (Fin 1)) (⟨x, h⟩ : ((Metric.sphere (0 : EuclideanSpace ℝ (Fin 2)) 1)))
@@ -67,12 +65,19 @@ theorem chartAt_source (y : Metric.sphere (0 : EuclideanSpace ℝ (Fin 2)) 1) :
     (chartAt (EuclideanSpace ℝ (Fin 1)) y).source = (stereographic' 1 (-y)).source := rfl
 
 def xh := ((⟨x, h⟩ :  Metric.sphere (0 : EuclideanSpace ℝ (Fin 2)) 1 ))
+def ug := ((⟨x, h⟩ :  Metric.sphere (0 : EuclideanSpace ℝ (Fin 2)) 1 ))
 
 theorem foo : (stereographic' 1 xh).source = {xh}ᶜ := stereographic'_source xh
 
-#check 1 ∉ {(1 : ℝ)}ᶜ
+theorem bar : {xh}ᶜ = { x | x ≠ xh } := rfl
 
--- theorem bar (a : ℝ) : a ∉ {a}ᶜ := sorry
+theorem baz : { x | x ≠ xh } = { a | (a.val 0)^2 + (a.val 1)^2 = 1 && (a.val 0, a.val 1) ≠ (x 0, x 1) }  := sorry
+theorem biz : { x | x ≠ ug } = { a | (a.val 0)^2 + (a.val 1)^2 = 1 && (a.val 0, a.val 1) ≠ (u 0, u 1) }  := sorry
+
+theorem bez : { y : Fin 2 -> ℝ | (y 0)^2 + (y 1)^2 = 1 && y 1 > 0 } ∩
+              { y : Fin 2 -> ℝ | (y 0)^2 + (y 1)^2 = 1 && y 1 < 0 }  = ∅ := sorry
+
+#check 1 ∉ {(1 : ℝ)}ᶜ
 
 example (cs : ChartedSpace (EuclideanSpace ℝ (Fin 1)) ((Metric.sphere (0 : EuclideanSpace ℝ (Fin 2)) 1))) (v : ((Metric.sphere (0 : EuclideanSpace ℝ (Fin 2)) 1))) :
   cs.chartAt v = stereographic' 1 (-v) := by exact sorry
@@ -83,13 +88,6 @@ def MyCoordChange : Fin 2 → Fin 2 → (Metric.sphere (0 : EuclideanSpace ℝ (
       | 0, 1, x, α => if (x.val 1) > 0 then α else -α
       | 1, 0, x, α => if (x.val 1) > 0 then α else -α
       | 1, 1, _, α => α
-
-#synth ChartedSpace (EuclideanSpace ℝ (Fin 1)) Circle
-#check (ChartedSpace.chartAt _).source
-#check (ChartedSpace.chartAt : Circle → PartialHomeomorph Circle (EuclideanSpace ℝ (Fin 1)))
-#check U.source
-#check finrank_real_complex_fact'
-#check Complex
 
 instance : Fact (Module.finrank ℝ ℂ = 1 + 1) := finrank_real_complex_fact'
 
@@ -103,33 +101,39 @@ theorem MyCoordChange_self : ∀ (i : Fin 2),
         | 1 => rfl
     exact h
 
-
 noncomputable
- def foog : (Metric.sphere (0 : EuclideanSpace ℝ (Fin 2)) 1) → EuclideanSpace ℝ (Fin 1) → EuclideanSpace ℝ (Fin 1)
+def f : (Metric.sphere (0 : EuclideanSpace ℝ (Fin 2)) 1) → EuclideanSpace ℝ (Fin 1) → EuclideanSpace ℝ (Fin 1)
    | x, α => if (x.val 1) > 0 then α else -α
 
-theorem foogg (x : (Metric.sphere (0 : EuclideanSpace ℝ (Fin 2)) 1)) (v : EuclideanSpace ℝ (Fin 1)) :
- MyCoordChange 1 0 x (MyCoordChange 0 1 x v) = v := by
-  have h3 : ∀ v, foog x v = MyCoordChange 0 1 x v := by
-   intro h
-   rfl
-  have h4 : ∀ v, foog x v = MyCoordChange 1 0 x v := by
-    intro h
-    rfl
-  have h5 : foog x (foog x v) = v := by
+lemma l (x : Metric.sphere (0 : EuclideanSpace ℝ (Fin 2)) 1) : f x (f x v) = v := by
     by_cases h : x.val 1 > 0
     case pos =>
-      simp [foog, h]
+      simp [f, h]
     case neg =>
-      simp [foog, h]
-  have h6 : foog x (foog x v) = foog x (MyCoordChange 0 1 x v) := by rw [h3 v]
-  have h7 : foog x (MyCoordChange 0 1 x v) = MyCoordChange 1 0 x (MyCoordChange 0 1 x v) := by rw [h4]
-  have h8 : foog x (foog x v) = MyCoordChange 1 0 x (MyCoordChange 0 1 x v) := by rw [h6, h7]
-  have h9 : MyCoordChange 1 0 x (MyCoordChange 0 1 x v) = v := by
-    calc
-      MyCoordChange 1 0 x (MyCoordChange 0 1 x v) = foog x (foog x v) := by rw [h8]
-      _ = v := h5
+      simp [f, h]
 
+theorem t1001 (x : (Metric.sphere (0 : EuclideanSpace ℝ (Fin 2)) 1)) (v : EuclideanSpace ℝ (Fin 1)) :
+ MyCoordChange 1 0 x (MyCoordChange 0 1 x v) = v := by
+  have h3 : ∀ v, f x v = MyCoordChange 0 1 x v := by
+   intro h
+   rfl
+  have h4 : ∀ v, f x v = MyCoordChange 1 0 x v := by
+    intro h
+    rfl
+  have h8 : f x (f x v) = MyCoordChange 1 0 x (MyCoordChange 0 1 x v) := by rw [h3 v, h4]
+  have h9 : MyCoordChange 1 0 x (MyCoordChange 0 1 x v) = v := by rw [<-h8, l]
+  exact h9
+
+theorem t0110 (x : (Metric.sphere (0 : EuclideanSpace ℝ (Fin 2)) 1)) (v : EuclideanSpace ℝ (Fin 1)) :
+ MyCoordChange 0 1 x (MyCoordChange 1 0 x v) = v := by
+  have h3 : ∀ v, f x v = MyCoordChange 0 1 x v := by
+   intro h
+   rfl
+  have h4 : ∀ v, f x v = MyCoordChange 1 0 x v := by
+    intro h
+    rfl
+  have h8 : f x (f x v) = MyCoordChange 1 0 x (MyCoordChange 0 1 x v) := by rw [h3 v, h4]
+  have h9 : MyCoordChange 1 0 x (MyCoordChange 0 1 x v) = v := by rw [<-h8, l]
   exact h9
 
 theorem MyCoordChange_comp : ∀ (i j k : Fin 2),
@@ -138,16 +142,14 @@ theorem MyCoordChange_comp : ∀ (i j k : Fin 2),
         (fun i => if i = 0 then chart_excluding_minus_1.source else chart_excluding_1.source) k,
     ∀ (v : EuclideanSpace ℝ (Fin 1)), MyCoordChange j k x (MyCoordChange i j x v) = MyCoordChange i k x v := by
     intro i j k x h v
-
-    have h1 : MyCoordChange 0 1 x (MyCoordChange 1 0 x v) = MyCoordChange 1 1 x v := sorry
     have h : MyCoordChange j k x (MyCoordChange i j x v) = MyCoordChange i k x v :=
       match i, j, k with
         | 0, 0, 0 => rfl
         | 0, 0, 1 => rfl
-        | 0, 1, 0 => foogg x v
+        | 0, 1, 0 => t1001 x v
         | 0, 1, 1 => rfl
         | 1, 0, 0 => rfl
-        | 1, 0, 1 => h1
+        | 1, 0, 1 => t0110 x v
         | 1, 1, 0 => rfl
         | 1, 1, 1 => rfl
     exact h
